7f646fe2818bf1b0fd6f4e99ff04ccea5114cccb
Tony <t4tonykuriakose@gmail.com>
2025-04-05 17:32:23 +0530

    feat:link with gateway

--- src/app/express/index.ts
+++ src/app/express/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 class Server {
   public app: Application;
@@ -7,6 +7,7 @@ class Server {
   constructor() {
     this.app = express();
     this.initialize();
+    this.setUpRoutes();
   }
 
   async initialize() {
@@ -15,6 +16,14 @@ class Server {
     } catch (error) {
       console.log("Error in initializing", error);
     }
+  }
+
+  setUpRoutes(){
+    this.app.get("/",(req:Request,res:Response)=>{
+
+      res.status(200).json({message:"Response from Notification API"})
+
+    })
   }
 
   start(PORT: number | string) {
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ config();
 
 
 
-
-
 const expressServer = new Server();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3006;
 expressServer.start(PORT);
 
